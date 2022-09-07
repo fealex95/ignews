b9022f4520cb6607aa043aa8557491d8ab2991cb
Felipe Alexandre <felipealexandre98@gmail.com>
2022-09-07 17:27:59 -0300

    Stripe e Subscription Completo

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { query as q } from 'faunadb';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
     providers: [
@@ -8,5 +11,40 @@ export default NextAuth({
             clientSecret: process.env.GITHUB_SECRET,
             scope: 'read:user'
         })
-    ]
+    ],
+    callbacks: {
+        async signIn(user, account, profile) {
+            const { email } = user.user;
+            try {
+                await fauna.query(
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('user_by_email'),
+                                    q.Casefold(email)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection('users', {
+                                data: { email }
+                            })
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(email)
+                            )
+                        )
+                    )
+                )
+
+                return true
+            } catch (e) {
+                return false
+            }
+
+        }
+    }
 })
